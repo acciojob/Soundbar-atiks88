b0abd50daabe57096ba063b36e18545b4ee5c4ad
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-10-24 19:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function playsound(soundFile) {
+	var audio = new Audio(soundFile);
+	audio.play();
+}
+
+document.getElementById("btn").addEventListner("click",function(){
+	playsound(sound1.mp3);
+	
+});
